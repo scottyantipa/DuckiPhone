5f0e27b87d927c2f376b8f1affec6370e478cff5
Scott Antipa <scottantipa@gmail.com>
2014-11-04 18:16:56 -0800

    forgot to add this delegate file

---
+++ Duck/StandardModalDelegate.h
@@ -0,0 +1,13 @@
+//
+//  StandardModalDelegate.h
+//  Duck
+//
+//  Created by Scott Antipa on 11/4/14.
+//  Copyright (c) 2014 Scott Antipa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol StandardModalDelegate <NSObject>
+-(void)didFinishEditing;
+@end
