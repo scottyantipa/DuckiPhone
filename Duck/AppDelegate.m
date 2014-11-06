a89e50421a0d1934975b380115a2dfd3b51b5b8f
Scott Antipa <scottantipa@gmail.com>
2014-11-05 18:41:14 -0800

    added options to persistent store for migration

--- Duck/AppDelegate.m
+++ Duck/AppDelegate.m
@@ -116,8 +116,12 @@
     }
     
     NSError *error = nil;
+    NSDictionary * storeOptions = @{
+                                    NSMigratePersistentStoresAutomaticallyOption: @YES,
+                                    NSInferMappingModelAutomaticallyOption: @YES
+                                    };
     _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:storeOptions error:&error]) {
         /*
          Replace this implementation with code to handle the error appropriately.
          
